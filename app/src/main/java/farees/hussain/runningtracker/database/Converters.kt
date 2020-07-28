82b656449afff95636b045796106da583f2a7bde
FareesHussain <fareezzhussain@gmail.com>
2020-07-28 19:18:06 +0530

    TypeConverters to convert Bitmap to ByteArray and viceversa

---
+++ app/src/main/java/farees/hussain/runningtracker/database/Converters.kt
@@ -0,0 +1,21 @@
+package farees.hussain.runningtracker.database
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    @TypeConverter
+    fun toBitmap(bytes : ByteArray) : Bitmap{
+        return BitmapFactory.decodeByteArray(bytes,0,bytes.size)
+    }
+
+    @TypeConverter
+    fun fromBitap(bmp : Bitmap): ByteArray{
+        val outputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG,100,outputStream)
+        return outputStream.toByteArray()
+    }
+}
