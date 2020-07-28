3b933d25248288694454aeb34edb544b1ae8f6c7
FareesHussain <fareezzhussain@gmail.com>
2020-07-28 18:48:25 +0530

    adding Entities and DatabaseDao

---
+++ app/src/main/java/farees/hussain/runningtracker/database/Run.kt
@@ -0,0 +1,18 @@
+package farees.hussain.runningtracker.database
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table" )
+data class Run(
+    var img : Bitmap?= null,
+    var timestamp: Long = 0L,
+    var avgSpeedInKMH : Float = 0F,
+    var distanceInKM : Int = 0,
+    var timeInMillis : Long = 0L,
+    var caloriesBurned : Int = 0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id : Int ?= null
+}
