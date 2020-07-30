772cad2ad9274c876016d32788064612fb252979
FareesHussain <fareezzhussain@gmail.com>
2020-07-30 19:08:15 +0530

    adding maps to mapView in TrackingFragment.kt

--- app/src/main/java/farees/hussain/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/farees/hussain/runningtracker/ui/fragments/TrackingFragment.kt
@@ -7,13 +7,54 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 import farees.hussain.runningtracker.R
 import farees.hussain.runningtracker.ui.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel : MainViewModel by viewModels()
+    private var map:GoogleMap ?= null
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView?.onCreate(savedInstanceState)
+
+        mapView?.getMapAsync{googleMap ->
+            map = googleMap
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 }
